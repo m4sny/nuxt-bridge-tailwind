29e54cc24d7c6a43e4dfab8e6e60c21ebadfab0e
m4sny <jimmz.pl@gmail.com>
2022-03-27 15:06:04 +0200

    init

---
+++ README.md
@@ -0,0 +1,121 @@
+```
+ Merging Tailwind config from ~/tailwind.config.js                                          nuxt:tailwindcss 14:34:49
+i Preparing project for development                                                                           14:34:51
+i Initial build may take a while                                                                              14:34:51
+i Discovered Components: .nuxt/components/readme.md                                                           14:34:51
+√ Builder initialized                                                                                         14:34:51
+i Directory components/ created. Restarting nuxt...                                                           14:34:51
+
+ FATAL  Cannot read properties of null (reading 'close')                                                      14:34:51
+
+  at Builder.unwatch (node_modules\@nuxt\builder-edge\dist\builder.js:666:30)
+  at Builder.close (node_modules\@nuxt\builder-edge\dist\builder.js:674:10)
+  at node_modules\@nuxt\builder-edge\dist\builder.js:195:42
+  at node_modules\hable\dist\hable.js:1:990
+  at node_modules\hable\dist\hable.js:1:208
+  at processTicksAndRejections (node:internal/process/task_queues:96:5)
+  at async Nuxt.callHook (node_modules\hable\dist\hable.js:1:959)
+  at async Nuxt.close (node_modules\@nuxt\core-edge\dist\core.js:363:5)
+  at async load (/F:/Dev/Private/nuxt-bridge/node_modules/nuxi/dist/chunks/dev.mjs:6748:11)
+  at async _applyPromised (/F:/Dev/Private/nuxt-bridge/node_modules/nuxi/dist/chunks/dev.mjs:6696:10)
+
+i Merging Tailwind config from ~/tailwind.config.js                                          nuxt:tailwindcss 14:34:51
+√ Nuxt files generated                                                                                        14:34:51
+
+✖ Client
+  Compiled with some errors in 346.87ms
+
+i Preparing project for development                                                                           14:34:52
+i Initial build may take a while                                                                              14:34:52
+i Discovered Components: .nuxt/components/readme.md                                                           14:34:52
+√ Builder initialized                                                                                         14:34:52
+√ Nuxt files generated                                                                                        14:34:52
+
+ ERROR  Failed to compile with 1 errors                                                       friendly-errors 14:34:52
+
+This dependency was not found:                                                                friendly-errors 14:34:52
+                                                                                              friendly-errors 14:34:52
+* F:\Dev\Private\nuxt-bridge\.nuxt\client.js in multi ./node_modules/eventsource-polyfill/dist/browserify-eventsource.js (webpack)-hot-middleware/client.js?reload=true&timeout=30000&ansiColors=&overlayStyles=&path=%2F__webpack_hmr%2Fclient&name=client ./.nuxt/client.js
+                                                                                              friendly-errors 14:34:52
+To install it, you can run: npm install --save F:\Dev\Private\nuxt-bridge\.nuxt\client.js     friendly-errors 14:34:52
+i Tailwind Viewer: /_tailwind/                                                               nuxt:tailwindcss 14:34:52
+
+ ERROR  Watchpack Error (initial scan): Error: ENOTDIR: not a directory, scandir 'F:\Dev\Private\nuxt-bridge\.nuxt\client.js'
+
+
+✔ Client
+  Compiled successfully in 3.60s
+
+i Waiting for file changes                                                                                    14:35:02
+Nuxt CLI v3.0.0-27470397.9ebea90                                                                              14:35:02
+                                                                                                              14:35:02
+  > Local:    http://localhost:3000/
+  > Network:  http://192.168.50.250:3000/
+
+i Directory components/ created. Restarting nuxt...                                                           14:35:02
+i Merging Tailwind config from ~/tailwind.config.js                                          nuxt:tailwindcss 14:35:03
+i Preparing project for development                                                                           14:35:03
+i Initial build may take a while                                                                              14:35:03
+i Discovered Components: .nuxt/components/readme.md                                                           14:35:03
+√ Builder initialized                                                                                         14:35:03
+
+ ERROR  Watchpack Error (initial scan): Error: ENOTDIR: not a directory, scandir 'F:\Dev\Private\nuxt-bridge\.nuxt\client.js'
+
+√ Nuxt files generated                                                                                        14:35:03
+√ Nitro built in 2813 ms                                                                                      14:35:05
+
+ ERROR  [worker] Cannot find module 'F:\Dev\Private\nuxt-bridge\.nuxt\views\document.template.mjs' imported from F:\Dev\Private\nuxt-bridge\.nuxt\nitro\index.mjs
+
+  at new NodeError (node:internal/errors:371:5)
+  at finalizeResolution (node:internal/modules/esm/resolve:418:11)
+  at moduleResolve (node:internal/modules/esm/resolve:981:10)
+  at defaultResolve (node:internal/modules/esm/resolve:1078:11)
+  at ESMLoader.resolve (node:internal/modules/esm/loader:530:30)
+  at ESMLoader.getModuleJob (node:internal/modules/esm/loader:251:18)
+  at ModuleWrap.<anonymous> (node:internal/modules/esm/module_job:79:40)
+  at link (node:internal/modules/esm/module_job:78:36)
+
+
+ WARN  [worker] Cannot find module 'F:\Dev\Private\nuxt-bridge\.nuxt\views\document.template.mjs' imported from F:\Dev\Private\nuxt-bridge\.nuxt\nitro\index.mjs
+
+  at new NodeError (node:internal/errors:371:5)
+  at finalizeResolution (node:internal/modules/esm/resolve:418:11)
+  at moduleResolve (node:internal/modules/esm/resolve:981:10)
+  at defaultResolve (node:internal/modules/esm/resolve:1078:11)
+  at ESMLoader.resolve (node:internal/modules/esm/loader:530:30)
+  at ESMLoader.getModuleJob (node:internal/modules/esm/loader:251:18)
+  at ModuleWrap.<anonymous> (node:internal/modules/esm/module_job:79:40)
+  at link (node:internal/modules/esm/module_job:78:36)
+
+
+● Client █████████████████████████ after emitting (98%)
+
+
+i Waiting for file changes                                                                                    14:35:10
+Nuxt CLI v3.0.0-27470397.9ebea90                                                                              14:35:10
+                                                                                                              14:35:10
+  > Local:    http://localhost:3000/
+  > Network:  http://192.168.50.250:3000/
+
+i Directory components/ created. Restarting nuxt...                                                           14:35:10
+i Merging Tailwind config from ~/tailwind.config.js                                          nuxt:tailwindcss 14:35:10
+i Preparing project for development                                                                           14:35:10
+i Initial build may take a while                                                                              14:35:10
+i Discovered Components: .nuxt/components/readme.md                                                           14:35:10
+√ Builder initialized                                                                                         14:35:10
+
+ ERROR  Failed to compile with 8 errors                                                       friendly-errors 14:35:10
+
+These dependencies were not found:                                                            friendly-errors 14:35:10
+                                                                                              friendly-errors 14:35:10
+* node:buffer in ./node_modules/ohmyfetch/node_modules/node-fetch/src/index.js                friendly-errors 14:35:10
+* node:http in ./node_modules/ohmyfetch/node_modules/node-fetch/src/index.js                  friendly-errors 14:35:10
+* node:https in ./node_modules/ohmyfetch/node_modules/node-fetch/src/index.js                 friendly-errors 14:35:10
+* node:net in ./node_modules/ohmyfetch/node_modules/node-fetch/src/utils/referrer.js          friendly-errors 14:35:10
+* node:stream in ./node_modules/ohmyfetch/node_modules/node-fetch/src/index.js                friendly-errors 14:35:10
+* node:url in ./node_modules/ohmyfetch/node_modules/node-fetch/src/request.js                 friendly-errors 14:35:10
+* node:util in ./node_modules/ohmyfetch/node_modules/node-fetch/src/body.js                   friendly-errors 14:35:10
+* node:zlib in ./node_modules/ohmyfetch/node_modules/node-fetch/src/index.js                  friendly-errors 14:35:10
+                                                                                              friendly-errors 14:35:10
+To install them, you can run: npm install --save node:buffer node:http node:https node:net node:stream node:url node:util node:zlib
+```
---
+++ pages/index.vue
@@ -0,0 +1,415 @@
+<template>
+  <div class="dark">
+    <div class="flex flex-col min-h-screen pb-10">
+      <main class="flex-grow overflow-x-hidden">
+        <section class="relative z-40 w-full pt-10 pb-16 lg:pt-5">
+          <div
+            class="
+              relative
+              z-20
+              flex flex-col
+              items-start
+              justify-start
+              px-8
+              mx-auto
+              sm:items-center
+              max-w-7xl
+              xl:px-5
+            "
+          >
+            <div class="prose prose-zinc dark:prose-invert">
+              <p
+                class="ql-block"
+                data-block-id="block-db91d2df-0906-4d18-a466-9c74d5208267"
+              >
+                Now that you've enabled Time Tracking for this Space - we can
+                track <em>automatically</em> with a plethora of time tracking
+                apps. The following explains how to get started with the Chrome
+                Extension, Harvest, Toggl, Everhour, Clockify, Timely, and
+                Timeneye:
+              </p>
+              <h2
+                class="ql-heading"
+                data-block-id="block-dc9e7306-0578-4ff8-b4b3-e6b11cb23f3b"
+              >
+                Our Chrome Extension
+              </h2>
+              <hr>
+              <ol>
+                <li
+                  class="ql-list-item"
+                  data-list="ordered"
+                  data-block-id="block-5cc41400-0af0-4dd7-bf95-9aed772d2824"
+                >
+                  <span class="ql-ui" contenteditable="false"></span>Install the extension from the
+                  <a
+                    class="ql-link"
+                    href="#"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    >Chrome Web Store</a
+                  ><span data-void-detect="true">.</span>
+                </li>
+                <li
+                  class="ql-list-item"
+                  data-list="ordered"
+                  data-block-id="block-65d09935-6557-46e4-a32b-ea68ca61b1cc"
+                >
+                  <span class="ql-ui" contenteditable="false"></span>From the
+                  <em>Time Spent</em> window, simply select a task and begin
+                  tracking time.
+                </li>
+                <li
+                  class="ql-list-item"
+                  data-list="ordered"
+                  data-block-id="block-e2c57377-b14b-403c-86b4-cac159d1361e"
+                >
+                  <span class="ql-ui" contenteditable="false"></span>You can
+                  even start the extension's timer from back inside the
+                  task.
+                </li>
+              </ol>
+              <h2
+                class="ql-heading"
+                data-block-id="block-d5991392-cead-4cd4-9ac5-6ff0637ec7ec"
+              >
+                Enabling Harvest
+              </h2>
+              <ol>
+                <li
+                  class="ql-list-item"
+                  data-list="ordered"
+                  data-block-id="block-6254afaa-337f-4128-8958-19f8c49a926a"
+                >
+                  <span class="ql-ui" contenteditable="false"></span>Go to
+                  Settings
+                </li>
+                <li
+                  class="ql-list-item"
+                  data-list="ordered"
+                  data-block-id="block-4fea30f5-69a2-4d18-99f1-930234b31391"
+                >
+                  <span class="ql-ui" contenteditable="false"></span>Click 'Time
+                  Tracking'
+                </li>
+                <li
+                  class="ql-list-item"
+                  data-list="ordered"
+                  data-block-id="block-c93217ae-e6e3-4ed4-bad1-9bc0e1665ce0"
+                >
+                  <span class="ql-ui" contenteditable="false"></span>Click the
+                  toggle switch to enable Harvest
+                </li>
+              </ol>
+              <h2
+                class="ql-heading"
+                data-block-id="block-773c5d76-7ac4-4ca8-a8f1-8206048ad14f"
+              >
+                Enabling Toggl
+              </h2>
+              <p
+                class="ql-block"
+                data-block-id="block-29d37faf-4138-46cd-98d8-3bc38cc4d4f4"
+              >
+                <strong>First</strong>, make sure you have the
+                <a
+                  class="ql-link"
+                  href="https://chrome.google.com/webstore/detail/toggl-button-productivity/oejgccbfbmkkpaidnkphaiaecficdnfn?hl=en"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  >Toggl Chrome extension</a
+                ><span data-void-detect="true">.</span>
+              </p>
+              <p
+                class="ql-block"
+                data-block-id="block-637c0691-3f05-4bce-898d-faa8a7b763a5"
+              >
+                <strong>Second</strong>, make sure you
+                <a
+                  class="ql-link"
+                  href="#features/time-tracking"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  >enable time tracking</a
+                ><span data-void-detect="true">
+                  in your Spaces where you want to track time. Note that an </span
+                ><a
+                  class="ql-link"
+                  href="#general/user-permissions"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  >admin</a
+                ><span data-void-detect="true"> must do this.</span>
+              </p>
+              <p
+                class="ql-block"
+                data-block-id="block-83c412db-09e8-4b79-a8c6-e286eafdfa7c"
+              >
+                <br />
+              </p>
+              <p
+                class="ql-block"
+                data-block-id="block-cfe1e994-f2b4-4c94-ace3-eae7b6bf3079"
+              >
+                Now, when you go back to a dsadas task, you should see the
+                Toggl button visible within your task!
+              </p>
+              <p
+                class="ql-block"
+                data-block-id="block-45fb4771-a1c7-40b5-93a9-095dc1ce5323"
+              >
+                <img
+                  class="ql-img"
+                  loading="lazy"
+                  src="https://downloads.intercomcdn.com/i/o/39756208/d880388ecff39e4c04c759f2/Screen+Shot+2017-11-16+at+3.09.38+PM.png"
+                />
+              </p>
+              <p
+                class="ql-block"
+                data-block-id="block-fa63dfa3-8054-4727-b614-4423744c18fd"
+              >
+                Simply click this button to start (or stop) tracking time in
+                Toggl! <strong> </strong>
+              </p>
+              <p
+                class="ql-block"
+                data-block-id="block-b54adc74-5868-4c48-831b-cf0bdbb49ae2"
+              >
+                <br />
+              </p>
+              <h2
+                class="ql-heading"
+                data-block-id="block-21759f46-8e04-4d35-a53d-fbfd44b189e9"
+              >
+                <strong>Enabling Everhour</strong>
+              </h2>
+              <ol>
+                <li
+                  class="ql-list-item"
+                  data-list="ordered"
+                  data-block-id="block-5ff7cf1d-f6eb-447b-a79e-d280884d8547"
+                >
+                  <span class="ql-ui" contenteditable="false"></span>Visit
+                  <a
+                    class="ql-link"
+                    href="https://everhour.com/integrations/dsadas"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    >this page</a
+                  ><span data-void-detect="true">
+                    to connect Everhour to your dsadas account</span
+                  >
+                </li>
+                <li
+                  class="ql-list-item ql-indent-1"
+                  data-list="bullet"
+                  data-block-id="block-275c09ce-7272-41de-9ec4-0d8047d7d4fa"
+                >
+                  <span class="ql-ui" contenteditable="false"></span
+                  ><em
+                    >If you do not have an Everhour account, their instructions
+                    will guide you through the simple setup process.</em
+                  >
+                </li>
+              </ol>
+              <p
+                class="ql-block"
+                data-block-id="block-a3c809f0-0060-486c-b15b-6cbcbfe2201c"
+              >
+                2. Install the
+                <a
+                  class="ql-link"
+                  href="https://chrome.google.com/webstore/detail/everhour-time-tracker/dnebklifojaaecmheejjopgjdljebpeo"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  >Everhour Extension</a
+                >
+              </p>
+              <ol>
+                <li
+                  class="ql-list-item ql-indent-1"
+                  data-list="bullet"
+                  data-block-id="block-0432bd9d-0929-4172-8d19-bce26f50802e"
+                >
+                  <span class="ql-ui" contenteditable="false"></span
+                  ><em>If you are prompted with the </em>Log in Everhour<em>
+                    screen in the extension, ignore the API Key field and click
+                    the link for </em
+                  >profile settings<em>
+                    . The extension will associate with your account.</em
+                  >
+                </li>
+              </ol>
+              <p
+                class="ql-block"
+                data-block-id="block-dc74c369-a93f-46bd-8bd3-26fc0647bd9f"
+              >
+                3. Open a task in dsadas to begin tracking time
+              </p>
+              <p
+                class="ql-block"
+                data-block-id="block-c07d9fcb-d9ad-42a9-9eda-d27f3f4f3377"
+              >
+                <br />
+              </p>
+              <h2
+                class="ql-heading"
+                data-block-id="block-76d87694-775c-49da-98a1-a791a50be0e7"
+              >
+                Enabling Clockify
+              </h2>
+              <ol>
+                <li
+                  class="ql-list-item"
+                  data-list="ordered"
+                  data-block-id="block-dcca1f06-68ab-4fe2-a62e-1bb03be121b8"
+                >
+                  <span class="ql-ui" contenteditable="false"></span
+                  ><a
+                    class="ql-link"
+                    href="https://clockify.me/signup"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    >Create a Clockify account</a
+                  >
+                </li>
+                <li
+                  class="ql-list-item"
+                  data-list="ordered"
+                  data-block-id="block-a1b12996-a358-42e3-bdcd-3aa6ec64baa3"
+                >
+                  <span class="ql-ui" contenteditable="false"></span>Install the
+                  <a
+                    class="ql-link"
+                    href="https://chrome.google.com/webstore/detail/clockify-time-tracker/pmjeegjhjdlccodhacdgbgfagbpmccpe"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    >Clockify Chrome extension</a
+                  >
+                </li>
+                <li
+                  class="ql-list-item"
+                  data-list="ordered"
+                  data-block-id="block-af829635-d84f-404c-bcb5-77c6db31154c"
+                >
+                  <span class="ql-ui" contenteditable="false"></span>Enable the
+                  <a
+                    class="ql-link"
+                    href="#features/clickapps"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    >Time Tracking ClickApp</a
+                  >
+                </li>
+                <li
+                  class="ql-list-item"
+                  data-list="ordered"
+                  data-block-id="block-4971ab43-a791-437b-aaed-53306fff2922"
+                >
+                  <span class="ql-ui" contenteditable="false"></span>Start the
+                  timer inside of your dsadas task. Note that the Clockify
+                  timer will appear automatically.
+                </li>
+              </ol>
+              <p
+                class="ql-block"
+                data-block-id="block-38ca0323-ef5d-4907-9b72-8d00cb7615f3"
+              >
+                <br />
+              </p>
+              <h2
+                class="ql-heading"
+                data-block-id="block-597e909d-4d85-4d06-875e-42259e195289"
+              >
+                Enabling Timely
+              </h2>
+              <ol>
+                <li
+                  class="ql-list-item"
+                  data-list="ordered"
+                  data-block-id="block-aa8ceeb3-d436-4b9a-b410-dd930b3da225"
+                >
+                  <span class="ql-ui" contenteditable="false"></span
+                  ><a
+                    class="ql-link"
+                    href="https://app.timelyapp.com/join"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    >Create a free Timely account</a
+                  >
+                </li>
+                <li
+                  class="ql-list-item"
+                  data-list="ordered"
+                  data-block-id="block-cc6e1a70-ff2e-4c05-bb68-883768ece7b5"
+                >
+                  <span class="ql-ui" contenteditable="false"></span>Connect the
+                  Memory Tracker app from Timely’s Hours view to start tracking
+                  time in dsadas.
+                </li>
+              </ol>
+              <p
+                class="ql-block"
+                data-block-id="block-0bea4f7b-7d45-42a1-ae72-f27c738644cc"
+              >
+                <br />
+              </p>
+              <h2
+                class="ql-heading"
+                data-block-id="block-98500f34-f985-4f55-90eb-482d2bae4c6b"
+              >
+                Enabling Timeneye
+              </h2>
+              <ol>
+                <li
+                  class="ql-list-item"
+                  data-list="ordered"
+                  data-block-id="block-eef417d0-729d-4986-ae7d-b177b7fdcb15"
+                >
+                  <span class="ql-ui" contenteditable="false"></span
+                  ><a
+                    class="ql-link"
+                    href="https://dash.readme.io/to/timeneye?redirect=docs/sign-up-and-password"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    >Create a Timeneye account</a
+                  >
+                </li>
+                <li
+                  class="ql-list-item"
+                  data-list="ordered"
+                  data-block-id="block-56adab4c-6869-4408-8322-5ddf487322d8"
+                >
+                  <span class="ql-ui" contenteditable="false"></span>Connect
+                  your dsadas account to Timeneye.<strong> </strong>Admins can
+                  do this from the <strong>Integrations </strong>section of
+                  their Timeneye account.
+                </li>
+                <li
+                  class="ql-list-item"
+                  data-list="ordered"
+                  data-block-id="block-35c0f516-ca05-4de2-85b1-757d2eb55513"
+                >
+                  <span class="ql-ui" contenteditable="false"></span>Import and
+                  link your dsadas Projects and users.
+                </li>
+              </ol>
+              <p
+                class="ql-block"
+                data-block-id="block-ead9dee1-e09d-4e23-b9ea-83cad41762ec"
+              >
+                <br />
+              </p>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage"
+};
+</script>
